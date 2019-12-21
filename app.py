1db0f2b6c2f34bf5e0e8de8beca508589ca32c03
jrathod9 <lewisburnard@gmail.com>
2019-12-21 19:29:26 +0530

    Login page initialised

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import os
+import json
+
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
